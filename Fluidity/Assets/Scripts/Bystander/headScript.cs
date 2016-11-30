ae1a179ca9f3dc8938ad5a777244794dc0c093a3
avvie <avatar.of.virtues@googlemail.com>
2016-11-30 14:41:16 +0200

    First COmmit

---
+++ Fluidity/Assets/Scripts/Bystander/headScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class headScript : MonoBehaviour {
+	public GameObject Front, Back; //hopefully not needed
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
